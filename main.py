42e59065847fad772d8919eaede5e7e5c9da5652
Brandon Bondig <brandon@bondig.dk>
2023-12-07 17:14:40 +0100

    terter

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ async def convert_doc_to_pdf(file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='172.26.4.219')
+    uvicorn.run(app, port=8080, host='localhost')
